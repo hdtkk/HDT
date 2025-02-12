3e654a9f08985ad1adbb399f6e16a21446914aa8
hdtkk <alocasia.feng@gmail.com>
2025-02-12 14:35:29 +0800

    Add files via upload

---
+++ Stage2_target_generation/vqgan.py
@@ -0,0 +1,79 @@
+import torch
+import torch.nn as nn
+from EncDecoder import Encoder, Decoder
+from codebook import Codebook
+import argparse
+
+class VQGAN(nn.Module):
+    def __init__(self, args):
+        super(VQGAN, self).__init__()
+        self.encoder = Encoder(args).to(device=args.device)
+        self.decoder = Decoder(args).to(device=args.device)
+        self.codebook = Codebook(args).to(device=args.device)
+        self.quant_conv = nn.Conv1d(args.latent_dim, args.latent_dim, 1).to(device=args.device)
+        self.post_quant_conv = nn.Conv1d(args.latent_dim, args.latent_dim, 1).to(device=args.device)
+
+    def forward(self, imgs):
+        encoded_images = self.encoder(imgs)
+        quant_conv_encoded_images = self.quant_conv(encoded_images)
+        codebook_mapping, codebook_indices, q_loss = self.codebook(quant_conv_encoded_images)
+        post_quant_conv_mapping = self.post_quant_conv(codebook_mapping)
+        decoded_images = self.decoder(post_quant_conv_mapping)
+
+        return decoded_images, codebook_indices, q_loss
+
+    def encode(self, imgs):
+        encoded_images = self.encoder(imgs)
+        quant_conv_encoded_images = self.quant_conv(encoded_images)
+        codebook_mapping, codebook_indices, q_loss = self.codebook(quant_conv_encoded_images)
+        return codebook_mapping, codebook_indices, q_loss
+
+    def decode(self, z):
+        post_quant_conv_mapping = self.post_quant_conv(z)
+        decoded_images = self.decoder(post_quant_conv_mapping)
+        return decoded_images
+
+    def calculate_lambda(self, perceptual_loss, gan_loss):
+        last_layer = self.decoder.model[-1]
+        last_layer_weight = last_layer.weight
+        perceptual_loss_grads = torch.autograd.grad(perceptual_loss, last_layer_weight, retain_graph=True)[0]
+        gan_loss_grads = torch.autograd.grad(gan_loss, last_layer_weight, retain_graph=True)[0]
+
+        λ = torch.norm(perceptual_loss_grads) / (torch.norm(gan_loss_grads) + 1e-4)
+        λ = torch.clamp(λ, 0, 1e4).detach()
+        return 0.8 * λ
+
+    @staticmethod
+    def adopt_weight(disc_factor, i, threshold, value=0.):
+        if i < threshold:
+            disc_factor = value
+        return disc_factor
+
+    def load_checkpoint(self, path):
+        self.load_state_dict(torch.load(path))
+
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description="VQGAN")
+    parser.add_argument('--latent-dim', type=int, default=256, help='Latent dimension n_z (default: 256)')
+    parser.add_argument('--image-size', type=int, default=256, help='Image height and width (default: 256)')
+    parser.add_argument('--num-codebook-vectors', type=int, default=1024, help='Number of codebook vectors (default: 256)')
+    parser.add_argument('--beta', type=float, default=0.25, help='Commitment loss scalar (default: 0.25)')
+    parser.add_argument('--image-channels', type=int, default=3, help='Number of channels of images (default: 3)')
+    parser.add_argument('--dataset-path', type=str, default='/data', help='Path to data (default: /data)')
+    parser.add_argument('--device', type=str, default="cuda", help='Which device the training is on')
+    parser.add_argument('--batch-size', type=int, default=6, help='Input batch size for training (default: 6)')
+    parser.add_argument('--epochs', type=int, default=100, help='Number of epochs to train (default: 50)')
+    parser.add_argument('--learning-rate', type=float, default=2.25e-05, help='Learning rate (default: 0.0002)')
+    parser.add_argument('--beta1', type=float, default=0.5, help='Adam beta param (default: 0.0)')
+    parser.add_argument('--beta2', type=float, default=0.9, help='Adam beta param (default: 0.999)')
+    parser.add_argument('--disc-start', type=int, default=10000, help='When to start the discriminator (default: 0)')
+    parser.add_argument('--disc-factor', type=float, default=1., help='')
+    parser.add_argument('--rec-loss-factor', type=float, default=1., help='Weighting factor for reconstruction loss.')
+    parser.add_argument('--perceptual-loss-factor', type=float, default=1., help='Weighting factor for perceptual loss.')
+
+    args = parser.parse_args()
+    args.dataset_path = r"C:\Users\dome\datasets\flowers"
+
+    train_vqgan = TrainVQGAN(args)
